0594467735daa321919aa9e65e6b05d7712f6f45
rlatlsdn98 <rlatlsdn98@naver.com>
2026-03-24 11:14:09 +0900

    init commit

---
+++ src/main/java/com/ohgiraffers/team3backendhr/Team3BackendHrApplication.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.team3backendhr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team3BackendHrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Team3BackendHrApplication.class, args);
+    }
+
+}
